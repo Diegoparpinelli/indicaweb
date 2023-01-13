ecd6205f0eefe69433e12004a03c73a2d979cfc4
Diegoparpinelli <78813378+Diegoparpinelli@users.noreply.github.com>
2023-01-13 15:43:03 -0400

    Add files via upload

---
+++ precache-manifest.a36aba59946c0b92b6a2ba1c3b5dabfe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9691fc85e9b54a86d0a042e49dffaa70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85ea2dacf245dd0e4fb",
+    "url": "/static/css/main.970a1490.chunk.css"
+  },
+  {
+    "revision": "685da7a8f27ac9954668",
+    "url": "/static/js/2.4be111ff.chunk.js"
+  },
+  {
+    "revision": "d5290ee2577431c7a548eb175e14a238",
+    "url": "/static/js/2.4be111ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e70e9887068f5617313b",
+    "url": "/static/js/3.ccf2fdb8.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.ccf2fdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73600f921d6d96a47c9c",
+    "url": "/static/js/4.9d0c3015.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.9d0c3015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655f66cdb162e01d1c88",
+    "url": "/static/js/5.6aa68c49.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.6aa68c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85ea2dacf245dd0e4fb",
+    "url": "/static/js/main.b18ca9d7.chunk.js"
+  },
+  {
+    "revision": "2438484f0a65636edfb3",
+    "url": "/static/js/runtime-main.a217f2f2.js"
+  },
+  {
+    "revision": "decdeb2452080e2512b0d1e445f2f193",
+    "url": "/static/media/PNF.decdeb24.gif"
+  },
+  {
+    "revision": "8387f118c6d8f218a6b86d66a4c9e2ab",
+    "url": "/static/media/empty_gif.8387f118.gif"
+  },
+  {
+    "revision": "bd2245f7a903dffff97200e082302f2b",
+    "url": "/static/media/icon.bd2245f7.png"
+  },
+  {
+    "revision": "31cc65133f8b38fdb72286d528f20361",
+    "url": "/static/media/logo.31cc6513.png"
+  },
+  {
+    "revision": "ca863f0b83485a8a7d4f6453cbce2986",
+    "url": "/static/media/top_10.ca863f0b.png"
+  }
+]);
